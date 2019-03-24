2b067d0823c9b846ca898c9b3e77d71478f53bf7
VSrednikh <VSrednikh>
2019-03-24 06:01:48 +0300

    Добавлен FrontolServiceIni

--- SharedModules/FrontolSO/FrontolSO/fmuServiceIni.cs
+++ SharedModules/FrontolSO/FrontolSO/fmuServiceIni.cs
@@ -9,8 +9,48 @@ using System.Windows.Forms;
 
 namespace FrontolSO
 {
+    public struct TServiceParams {
+        string Ini;
+        string IniSection;
+        string SvcName;
+        string DisplayName;
+        string Log;
+        string LogSection;
+    }
+
+
+
+
+
     public partial class fmuServiceIni : Form
     {
+
+        protected
+          string fIniLogFile;
+
+        private
+          //THandle fSrvHandle; 
+         // byte fLastState;
+        const string USER_LOCAL_SYSTEM = "LocalSystem";
+        const string PASS_UNASSIGNED = "PASS_UNASSIGNED";
+        private void LoadDBParams()
+        {
+            #region
+
+            #endregion
+        }
+        private void SaveDBParams()
+        {
+            #region
+            string ini = GetServiceIniPath();
+            #endregion
+        }
+        private string GetServiceIniPath() {
+            #region
+            return "";
+            #endregion
+        }
+
         public fmuServiceIni()
         {
             InitializeComponent();
